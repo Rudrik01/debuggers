f3317e72463aaf23b53de0df46c8ee714ffd99f6
Dharmi05 <129818960+Dharmi05@users.noreply.github.com>
2023-04-28 12:22:44 +0530

    Defined other member function

--- TowerofHanoi.cpp
+++ TowerofHanoi.cpp
@@ -173,3 +173,157 @@ void Game::Hint(){
     }
     }
 }
+void Game::Buycoins(){
+    //PAY
+    int count = 0;
+    gotoxy(10,6);
+    cout << "How many coins do you want ?";
+    gotoxy(10,6);
+    cin >> count;
+    users[name] += count;
+}
+void Game::updateScore(){
+	gotoxy(WIN_WIDTH + 7, 5);cout<<"Tries: "<<tries<<endl;
+}
+void Game::instructions(){
+	gotoxy(10,6);cout << "Read Carefully!!!!!" << endl;
+	system("pause");
+	
+	system("cls");
+	cout<<"Instructions";
+	cout<<"\n----------------";
+	cout<<"\n Shift Disks from Tower 1 to Tower 3. ";
+	cout<<"\n You can not place large disk on small disk";
+	cout<<"\n Towers are Numbered as 1,2 and 3" << endl;
+}
+void Game::drawTile(int tower, int tileNo, int y){
+	int x;
+	if( tower == 1 ) x = T1POS;
+	else if( tower == 2 ) x = T2POS;
+	else if( tower == 3 ) x = T3POS;
+	
+	x -= tileNo;
+	
+	for(int j=0; j<((tileNo)*2)-1; j++){
+		gotoxy(x,y);
+		cout<<"±";
+		x++;
+	}
+}
+void Game::drawTower(int tower){
+	//int x;
+	int y = 9;
+	
+	gotoxy(10, 10); cout<<"==========";
+	gotoxy(25, 10); cout<<"==========";
+	gotoxy(40, 10); cout<<"==========";
+	
+	gotoxy(15, 11); cout<<"1";
+	gotoxy(30, 11); cout<<"2";
+	gotoxy(45, 11); cout<<"3";
+	
+	for(int i=0; i<5; i++){
+		drawTile(tower, towers[tower-1][i], y);
+		y--;
+	}
+}
+int Game::isEmpty(int towerNo){
+	for(int i=0; i<DISKS; i++)
+		if( towers[towerNo][i] != 0 )
+			return 0;
+	return 1;
+}
+int Game::validate(int from, int to){
+	if( !isEmpty(to) ){
+		  
+		if( towers[from][towerTop[from]] < towers[to][towerTop[to]] )
+			return 1;
+		else 
+			return 0;
+	}
+	return 1;
+}
+int Game::move(int from, int to){
+	if( isEmpty(from) ) return 0; 
+	if( validate(from, to) ){ 
+		if( towers[from][towerTop[from]] != 0 ){
+			towerTop[to]++;
+			towers[to][towerTop[to]] = towers[from][towerTop[from]];
+			towers[from][towerTop[from]] = 0;
+			towerTop[from]--;
+			return 1;
+		}
+	}
+	return 0;
+}
+int Game::win(){ 
+	for(int i=0; i<DISKS; i++)
+		if( towers[2][i] != DISKS-i )
+			return 0;
+	return 1;
+}
+
+void Game::play(){
+	
+	gotoxy(10,6);cout << "GEAR UP YOUR SEAT BELT" << endl;
+	Sleep(900);
+	int from, to;
+	for(int i=0; i<DISKS; i++)
+		towers[0][i] = DISKS-i;
+	for(int i=0; i<DISKS; i++)
+		towers[1][i] = 0;
+	for(int i=0; i<DISKS; i++)
+		towers[2][i] = 0;
+	 
+	do{
+	
+		system("cls");  
+		
+		cout<<"============================================================"<<endl;
+		cout<<"                       TOWER OF HANOI                       "<<endl;
+		cout<<"============================================================"<<endl<<endl;
+		
+		drawTower(1);
+		drawTower(2);
+		drawTower(3);
+		
+		if( win() ){
+			system("cls");
+			cout<<"============================================================"<<endl;
+			cout<<"                           YOU WIN                          "<<endl;
+			cout<<"============================================================"<<endl;
+			cout<<endl<<endl<<endl;
+			cout<<"Press any key to go back to menu...";
+			getch();
+			break;
+		}
+		
+		gotoxy(10,15);
+		cout<<"From (Values: 1,2,3): ";
+		cin>>from;
+		gotoxy(10,16);
+		cout<<"To (Values: 1,2,3): ";
+		cin>>to;
+		
+		if( to < 1 || to > 3 ) continue;
+		if( from < 1 || from > 3 ) continue;
+		if( from == to ) continue;
+		
+		from--;
+		to--;
+		
+		move(from, to);
+		 
+		if(kbhit()){
+			char ch = getch();
+			if( ch=='a' || ch=='A' ){ 
+			}
+			if( ch=='d' || ch=='D' ){ 
+			} 
+			if(ch==27){
+				break;
+			}
+		} 
+		  
+	}while(1);
+}
